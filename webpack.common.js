2e6fb7945c0790c6ed801e4855fcb44dd5664fe9
Greg Pfaff <greg.pfaff@razorfish.com>
2018-11-12 07:16:37 -0600

    adding fixed test setup

--- webpack.common.js
+++ webpack.common.js
@@ -23,14 +23,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+        test: /\.js$/, exclude: /node_modules/, loaders: 'babel-loader'
       },
       {
         test: /\.s(a|c)ss$/,
